8dc711d043a9701a03d8cf0b4a774c1a0f82580e
18004885 <18004885@myrp.edu.sg>
2020-05-04 12:43:55 +0800

    KELLY PART DONE

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,17 +13,31 @@ import java.util.ArrayList;
 public class FirstActivity extends AppCompatActivity {
 
     ListView lv;
-    ArrayList<String> al;
+    ArrayList<String> al = new ArrayList<>();
     ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        al = new ArrayList<String>();
+        lv = findViewById(R.id.lvYear);
+        aa = new ArrayAdapter<>(this,R.layout.rowyear,R.id.tvYear,al);
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
 
 
